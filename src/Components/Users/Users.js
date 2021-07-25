043bef067bc4199cd36b26e39924c5a123b0a18b
aaron <mcmaaron@gmail.com>
2021-07-26 00:29:02 +0100

    Add loading spinner state and environment variables for HTTP requests

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,46 +1,31 @@
-import React, { Component } from "react";
-import UserItem from "./UserItem";
+import React from 'react';
+import UserItem from './UserItem';
+import PropTypes from 'prop-types';
+import Spinner from '../Layout/Spinner';
 
-
-class Users extends Component {
-  state = {
-    users: [
-      {
-        id: "1",
-        login: "aaronzor",
-        avatar_url: "https://avatars.githubusercontent.com/u/9366459?v=4",
-        html_url: "https://github.com/aaronzor",
-      },
-      {
-        id: "2",
-        login: "aaronzor2",
-        avatar_url: "https://avatars.githubusercontent.com/u/9366459?v=4",
-        html_url: "https://github.com/aaronzor",
-      },
-      {
-        id: "3",
-        login: "aaronzor3",
-        avatar_url: "https://avatars.githubusercontent.com/u/9366459?v=4",
-        html_url: "https://github.com/aaronzor",
-      },
-    ],
-  };
-
-  render() {
-    return (
-      <div style={userStyle}>
-        {this.state.users.map((user) => (
-          <UserItem key={user.id} user={user} />
-        ))}
-      </div>
-    );
-  }
-}
+const Users = ({ users, loading }) => {
+    if (loading) {
+        return <Spinner />;
+    } else {
+        return (
+            <div style={userStyle}>
+                {users.map((user) => (
+                    <UserItem key={user.id} user={user} />
+                ))}
+            </div>
+        );
+    }
+};
 
 const userStyle = {
-  display: 'grid',
-  gridTemplateColumns: 'repeat(3, 1fr)',
-  gridGap: '1rem'
-}
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gridGap: '1rem',
+};
+
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired,
+};
 
 export default Users;
